dfff0b2cc09c84c1270a1ccda35d7908022998cc
Jesus Jauregui <jesus.jauregui-c@mail.escuelaing.edu.co>
2023-08-19 12:03:31 -0500

    Cambio test.py

--- test.py
+++ test.py
@@ -4,14 +4,19 @@ import cplx_lib
 class Test_cplx_functions(unittest.TestCase):
 
     def test_suma(self):
-        # (3 + 2i) + (-5 + 5.2i) = (-2, 7.2i)
+        # 1. (3 + 2i) + (-5 + 5.2i) = (-2 , 7.2i)
         c1 = (3,2)
         c2 = (-5,5.2)
         self.assertAlmostEqual(cplx_lib.suma_cplx(c1,c2),(-2,7.2))
-        # (3 + 5i) + (-2.6 + 6.8i) = (0.4, 11.8i)
+        # 2. (3 + 5i) + (-2.6 + 6.8i) = (0.4, 11.8i)
         suma = cplx_lib.suma_cplx((3,5),(-2.6,6.8))
         self.assertAlmostEqual(suma[0], 0.4)
         self.assertAlmostEqual(suma[1], 11.8)
+    def test_resta(self):
+        # 1. (5.6 + -5i) - (-2 + 3.6i) = (7.6 , -8.6i)
+        a = (5.6,-5)
+        b = (-2,3.6)
+        self.assertAlmostEqual(cplx_lib.resta_cplx(a,b), (7.6,-8.6))
 
 if __name__ == '__main__':
     unittest.main()
